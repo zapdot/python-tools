39c46a3c22c8aec56953d12de1164cf957ef3c7d
Michael Carriere <mike@zapdot.com>
2017-05-18 17:16:32 -0400

    added target filter for daily cloud build

--- cron/ucb_daily.py
+++ cron/ucb_daily.py
@@ -5,10 +5,13 @@ import os.path
 from zaplib.api.cloudbuild import CloudBuildAPI
 from zaplib.api.github import GitHubAPI
 
-def get_dirty_targets(latest_hash):
+def get_dirty_targets(latest_hash, filter=None):
 	result = []
 
 	build_targets = [bt for bt in cb.get_buildtargets() if bt['enabled']]
+
+	if filter:
+		build_targets = [bt for bt in build_targets if filter in bt['buildtargetid']]
 
 	for bt in build_targets:
 		build_info = bt.get('build', None)
@@ -22,13 +25,17 @@ def get_dirty_targets(latest_hash):
 
 if __name__ == '__main__':
 
+	target_filter = None
+
 	## arguments
-	if len(sys.argv) != 2:
-		print "error: invalid arguments"
-		print "%s [CONFIGBOX_ID]\n" % os.path.basename(__file__)
-		sys.exit(-1)
-	else:
+	if len(sys.argv) == 2:
 		filename, cbox_id = sys.argv
+	elif len(sys.argv) == 3:
+		filename, cbox_id, target_filter = sys.argv
+	else:
+		print "error: invalid arguments"
+		print "%s [CONFIGBOX_ID] ([TARGET_FILTER])\n" % os.path.basename(__file__)
+		sys.exit(-1)
 
 	## setup
 	cb = CloudBuildAPI(cbox_id)
@@ -36,7 +43,7 @@ if __name__ == '__main__':
 
 	# get targets that need builds
 	develop_sha = github.commit("develop").sha
-	targets = get_dirty_targets(develop_sha)
+	targets = get_dirty_targets(develop_sha, target_filter)
 
 	for target in targets:
-		cb.create_build(target)
+		cb.create_build(target)				
