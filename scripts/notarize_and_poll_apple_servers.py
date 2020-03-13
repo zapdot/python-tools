b406a9af2b83fb5630f677d9892f2c65172be2b8
Mat MacKenzie <mat@zapdot.com>
2020-03-13 10:01:40 -0400

    Read from notarization report URL.
    Print contents of notarization report to log.

--- scripts/notarize_and_poll_apple_servers.py
+++ scripts/notarize_and_poll_apple_servers.py
@@ -8,6 +8,7 @@
 import subprocess
 import sys
 import time
+import urllib
 
 if len(sys.argv) != 5:
 	print "NotarizeAndPollAppleServers.py: need bundle, ID Apple ID, app password, and path to zip file."
@@ -20,7 +21,8 @@ appZipPath = sys.argv[4]
 
 print "NotarizeAndPollAppleServers.py: Starting notarization."
 uploadOutput = subprocess.check_output(['xcrun', 'altool', '--notarize-app', '--primary-bundle-id', bundleID, '-u', appleID, '-p', appSpecificPassword, '--file', appZipPath], stderr=subprocess.STDOUT)
-print "NotarizeAndPollAppleServers.py: upload output: ", uploadOutput
+print "NotarizeAndPollAppleServers.py: upload output: "
+print uploadOutput
 
 requestIDIndex = uploadOutput.find("RequestUUID = ")
 if requestIDIndex == -1:
@@ -33,16 +35,25 @@ print "Request ID: ", requestID
 # Every 15 minutes we poll the Apple servers for a result.
 while True:
 	pollOutput = subprocess.check_output(['xcrun', 'altool', '--notarization-info', requestID, '-u', appleID, '-p', appSpecificPassword], stderr=subprocess.STDOUT)
-	print "NotarizeAndPollAppleServers.py: poll output: ", pollOutput
+	print "NotarizeAndPollAppleServers.py: poll output: "
+	print pollOutput
 	
 	# check for successful notarization
-	pollOutput = pollOutput.lower()
-	if "package approved" in pollOutput:
+	pollOutputLower = pollOutput.lower()
+	if "package approved" in pollOutputLower:
 		print "NotarizeAndPollAppleServers.py: Notarization succeeded."
+		notarizationReportURLIndex = pollOutput.find("LogFileURL: ")
+		if notarizationReportURLIndex == -1:
+			print "NotarizeAndPollAppleServers.py: Unable to find notarization report URL in output from Apple notarization server."
+		else:
+			notarizationReportURL = pollOutput[notarizationReportURLIndex + len("LogFileURL: "):].splitlines()[0].strip()
+			notarizationReportContents = urllib.urlopen(notarizationReportURL).read()
+			print "NotarizeAndPollAppleServers.py: notarization report: "
+			print notarizationReportContents
 		exit(0)
 		
 	# check for failed notarization
-	elif "fail" in pollOutput or "error" in pollOutput and not "in progress" in pollOutput:
+	elif "fail" in pollOutput or "error" in pollOutputLower and not "in progress" in pollOutputLower:
 		print "NotarizeAndPollAppleServers.py: Notarization failed."
 		exit(-3)
 
